7a43594447c14861a24ad140ab8d6b75ca6ae485
AsofiaOrt <159494048+AsofiaOrt@users.noreply.github.com>
2024-07-11 22:30:30 -0500

    single line

--- spi_configSKZ.v
+++ spi_configSKZ.v
@@ -79,27 +79,17 @@ module spi_config(
 	case(state) 
 		INIT:begin //configuracion 
 			case(count)
-			4'h0:begin  spistart<=1;	comm<=0; 
-			if (avail) count<=4'h1;
-			end
+			4'h0:begin  spistart<=1;	comm<=0; if (avail) count<=4'h1; end
+				
+			4'h1: begin message<=8'b00100001;if (avail) count<=4'h2; end
 			
-			4'h1: begin message<=8'b00100001;
-			if (avail) count<=4'h2;
-			end
+			4'h2:begin  message<=8'b10010000; if (avail) count<=4'h3; end
+			 
+			4'h3: begin message<=8'b00100000; if(avail) count<=4'h4; end
 			
-			4'h2:begin  message<=8'b10010000;	 
-			if (avail) count<=4'h3;
-			end
+			4'h4: begin message<=8'b00001100; if(avail) count<=4'h5; end
 			
-			4'h3: begin message<=8'b00100000; 
-			if(avail) count<=4'h4;
-			end
-			
-			4'h4: begin	message<=8'b00001100; 
-			if(avail) count<=4'h5;
-			end
-			
-			4'h5: begin	 comm<=1; message<=8'h0; //limpia la pantalla
+			4'h5: begin comm<=1; message<=8'h0; //limpia la pantalla
 			if(avail) begin 
 				if (i<=503) begin 
 					i<=i+1;
@@ -117,75 +107,47 @@ module spi_config(
 			endcase
 		end
 		
+		
 		SKZ: begin //dibujar
 			poss_x<=8'hA3;
 			poss_y<=8'h42;
 			back<=0;	
 			case(count)
-				4'h0: begin comm<=0; message<=poss_x;
-			if(avail) count<=4'h1;
-			end
 			
-			4'h1: begin  message<=poss_y; 
-			if(avail) count<=4'h2;
-			end
+			4'h0: begin comm<=0; message<=poss_x; if(avail) count<=4'h1; end
+			
+			4'h1: begin  message<=poss_y; if(avail) count<=4'h2; end
 			
 			4'h2: begin comm<=1; message<=8'b00001001;//mas entendible en binario
 			if(avail) count<=4'h3;
 			end
 			
-			4'h3: begin message<=8'b00010101;
-			if(avail) count<=4'h4;
-			end
+			4'h3: begin message<=8'b00010101; if(avail) count<=4'h4; end
 			
-			4'h4: begin message<=8'b00010101;
-			if(avail) count<=4'h5;
-			end
+			4'h4: begin message<=8'b00010101; if(avail) count<=4'h5; end
 			
-			4'h5: begin  message<=8'b00010010;
-			if(avail) count<=4'h6;
-			end
+			4'h5: begin  message<=8'b00010010;if(avail) count<=4'h6;end
 			
-			4'h6: begin comm<=0; poss_x<=poss_x+5; message<=poss_x; 
-			if(avail) count<=4'h7;
-			end
+			4'h6: begin comm<=0; poss_x<=poss_x+5; message<=poss_x; if(avail) count<=4'h7; end
 			
-			4'h7: begin comm<=1;	message<=8'b00011111;
-			if(avail) count<=4'h8;
-			end
+			4'h7: begin comm<=1;	message<=8'b00011111; if(avail) count<=4'h8; end
 			
-			4'h8: begin message<=8'b00000100;
-			if(avail) count<=4'h9;
-			end
+			4'h8: begin message<=8'b00000100; if(avail) count<=4'h9; end
 			
-			4'h9: begin message<=8'b00001010;
-			if(avail) count<=4'hA;
-			end
+			4'h9: begin message<=8'b00001010; if(avail) count<=4'hA; end
+						
+			4'hA: begin message<=8'b00010001; if(avail) count<=4'hB; end
 			
-			4'hA: begin message<=8'b00010001;
-			if(avail) count<=4'hB;
-			end
-			
-			4'hB: begin comm<=0; poss_x<=poss_x+5; message<=poss_x; 
-			if(avail) count<=4'hC;
-			end
+			4'hB: begin comm<=0; poss_x<=poss_x+5; message<=poss_x; if(avail) count<=4'hC; end
 
-			4'hC: begin message<=8'b00010011;
-			if(avail) count<=4'hD;
-			end
-
-			4'hD: begin message<=8'b00010101;
-			if(avail) count<=4'hE;
-			end
-
-			4'hE: begin message<=8'b00011001;
-				if(avail) count<=4'hF;
-			end
-
-			4'hF: begin message<=8'b00010001;
-				if(avail) spistart<=0;
-			end
+			4'hC: begin message<=8'b00010011; if(avail) count<=4'hD; end
 			
+			4'hD: begin message<=8'b00010101; if(avail) count<=4'hE; end
+			
+			4'hE: begin message<=8'b00011001; if(avail) count<=4'hF;end
+			
+			4'hF: begin message<=8'b00010001; if(avail) spistart<=0; end
+
 			endcase 
 		end
 	endcase
