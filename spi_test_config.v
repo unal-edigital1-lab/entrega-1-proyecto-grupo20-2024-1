0af952120f8c3405440e03262259e8b99d5f2bee
AsofiaOrt <159494048+AsofiaOrt@users.noreply.github.com>
2024-07-27 20:41:56 -0500

    Update spi_test_config.v

--- spi_test_config.v
+++ spi_test_config.v
@@ -7,15 +7,14 @@ module spi_test_config;
     wire mosi;
 
     // Outputs
-	 wire dc;
+    wire dc;
     wire sclk;
     wire sce;
-	 wire [7:0] message;
+    wire [7:0] message;
 	 
 	 spi_configBunny uut(
 		.clock(clk),
 		.Reset(reset),
-
 		.mosi(mosi),
 		.sclk(sclk),
 		.sce(sce),
@@ -23,24 +22,15 @@ module spi_test_config;
 		.message(message)
 	);
 	
-	
 	initial begin
         clk = 0;
         forever #1 clk = ~clk; //500Mhz
-    end
+   	end
 	 
 	initial begin
 		reset=0; //se presiona reset primero para reinicializar todo
 		#200;
 		reset=1;
-		
-		//$display("mosi %d", mosi);
-		
-		
-		//#10000;
-		//$stop;
-
 	end
-	
 	
 endmodule
